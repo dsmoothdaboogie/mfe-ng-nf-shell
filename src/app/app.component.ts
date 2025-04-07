df39224d23dae8a006161fd1719f92e9cae33eb4
dsmoothdaboogie <64094990+dsmoothdaboogie@users.noreply.github.com>
2025-04-07 18:14:02 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, NgZone, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'shell';
+
+  constructor() {
+    (globalThis as any).ngZone = inject(NgZone);
+
+  }
+}
+
---
+++ src/bootstrap.ts
@@ -0,0 +1,18 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { APP_ROUTES } from './app/app.routes';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      APP_ROUTES, 
+      withComponentInputBinding())
+    ],
+});
